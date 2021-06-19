40f04d2c2aa4df89c26e67a4217d4964f6366f4e
Viklysx <vikbelec111@yandex.ru>
2021-06-19 23:28:33 +0300

    feat: finish task

--- app/src/components/Menu.js
+++ app/src/components/Menu.js
@@ -1,3 +1,35 @@
+import { BrowserRouter as Router, NavLink, Route } from "react-router-dom";
+import HomePage from "./HomePage";
+import DriftPage from "./DriftPage";
+import TimeAttackPage from "./TimeAttackPage";
+import ForzaPage from "./ForzaPage";
+
 export default function Menu() {
-    return null;
+const activeClass = "menu__item-active";
+  return (
+    <>
+      <Router>
+        <nav class="menu">
+          <NavLink exact className="menu__item" activeClassName={activeClass} to="/">
+            Главная
+          </NavLink>
+          <NavLink className="menu__item" activeClassName={activeClass} to="/drift">
+            Дрифт-такси
+          </NavLink>
+          <NavLink className="menu__item" activeClassName={activeClass} to="/timeattack">
+            Time Attack
+          </NavLink>
+          <NavLink className="menu__item" activeClassName={activeClass} to="/forza">
+            Forza Karting
+          </NavLink>
+        </nav>
+        <div className="page">
+            <Route path="/" exact component={HomePage} />
+            <Route path="/drift" component={DriftPage} />
+            <Route path="/timeattack" component={TimeAttackPage} />
+            <Route path="/forza" component={ForzaPage} />
+        </div>
+      </Router>
+    </>
+  );
 }
