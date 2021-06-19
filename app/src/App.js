4d26085685530bb069d14b0cef49ef9fd701b287
Viklysx <vikbelec111@yandex.ru>
2021-06-19 23:06:14 +0300

    feat: add start components

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Menu from "./components/Menu";
+import HomePage from "./components/HomePage";
+import DriftPage from "./components/DriftPage";
+import TimeAttackPage from "./components/TimeAttackPage";
+import ForzaPage from "./components/ForzaPage";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Router>
+        <div>
+          <Menu />
+          <div className="page">
+            <Route path="/" exact component={HomePage} />
+            <Route path="/drift" component={DriftPage} />
+            <Route path="/timeattack" component={TimeAttackPage} />
+            <Route path="/forza" component={ForzaPage} />
+          </div>
+        </div>
+      </Router>
+    </>
   );
 }
 
